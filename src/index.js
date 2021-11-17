e7f18bff80440ae198edec9f5becf2a1ef784466
Anna Pittman <annaepittman@gmail.com>
2021-11-17 15:35:44 -0800

    create toLocaleString function
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- src/index.js
+++ src/index.js
@@ -13,12 +13,13 @@ $(document).ready(function() {
       const body = JSON.parse(response);
       console.log(body);
       for(let i=0; i<10; i++) {
-        $('#currencies').append(`<li>${body[i].name}<span id='${i}'></span></li>`);
+        $('#currencies').append(`<li>${body[i].name}<span></span></li>`);
       }
       $('#price').click(function() {
         $("span").each(function(i) {
           let roundedPrice = Math.round(`${body[i].price}` * 100) / 100;          
-          this.append(` Price: ` + roundedPrice);        
+          roundedPrice = roundedPrice.toLocaleString();
+          this.append(` - Price: ` + roundedPrice);        
         });
       });
     }, function(error) {
