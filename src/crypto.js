5074d8cbcf78d07697199bc96e7a777341515cef
Anna Pittman <annaepittman@gmail.com>
2021-11-18 11:10:46 -0800

    create exchange functionality
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- src/crypto.js
+++ src/crypto.js
@@ -14,4 +14,5 @@ export default class Crypto {
       request.send();
     });
   }
+
 }
--- src/index.js
+++ src/index.js
@@ -5,25 +5,46 @@ import './css/styles.css';
 import Crypto from './crypto.js';
 
 $(document).ready(function() {
+
+  let promise = Crypto.getCurrency();
+
   $('#list-currencies').click(function() {
-    // const type = "currencies/ticker";
-    let promise = Crypto.getCurrency();
-    console.log(promise);
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body);
-      for(let i=0; i<10; i++) {
+      for(let i=0; i<body.length; i++) {
         $('#currencies').append(`<li>${body[i].name}<span></span></li>`);
       }
       $('#price').click(function() {
         $("span").each(function(i) {
           let roundedPrice = Math.round(`${body[i].price}` * 100) / 100;          
           roundedPrice = roundedPrice.toLocaleString();
-          this.append(` - Price: ` + roundedPrice);        
+          this.append(` - Price: $` + roundedPrice + ` USD`);        
         });
       });
     }, function(error) {
       $('#errors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+  
+  $('#exchange').click(function() {
+    promise.then(function(response) {
+      const body2 = JSON.parse(response);
+      const input = $('#input').val();
+      const inputCurrency = $('#inputCurrency').val();
+      const outputCurrency = $('#outputCurrency').val();
+      for(let i=0; i<body2.length; i++) {
+        if(inputCurrency === `${body2[i].name}`) {
+          const inputPrice = `${body2[i].price}`;
+          for(let i=0; i<body2.length; i++) {
+            if(outputCurrency === `${body2[i].name}`) {
+              const outputPrice = `${body2[i].price}`;
+              const output = (inputPrice / outputPrice) * input;
+              $("#showExchange").append(output);
+              console.log(output);
+            }
+          }
+        }
+      }
     });
   });
 });
